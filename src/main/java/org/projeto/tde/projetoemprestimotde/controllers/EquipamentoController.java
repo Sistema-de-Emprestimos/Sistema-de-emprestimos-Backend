784217fb407c6e093e08871109533b14c3c624be
joaoarthur <joaoabritto.dev@gmail.com>
2024-09-16 01:17:44 -0300

    feat: Added equipamento controller and service

---
+++ src/main/java/org/projeto/tde/projetoemprestimotde/controllers/EquipamentoController.java
@@ -0,0 +1,54 @@
+package org.projeto.tde.projetoemprestimotde.controllers;
+
+import jakarta.validation.Valid;
+import org.projeto.tde.projetoemprestimotde.DTOs.Equipamento.CadastroEquipamentoDTO;
+import org.projeto.tde.projetoemprestimotde.DTOs.Equipamento.DetalheEquipamentoDTO;
+import org.projeto.tde.projetoemprestimotde.DTOs.Equipamento.EquipamentoDTO;
+import org.projeto.tde.projetoemprestimotde.DTOs.Equipamento.UpdateEquipamentoDTO;
+import org.projeto.tde.projetoemprestimotde.DTOs.Manutencao.ManutencaoDTO;
+import org.projeto.tde.projetoemprestimotde.services.EquipamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/equipamentos")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class EquipamentoController {
+
+    @Autowired
+    private EquipamentoService equipamentoService;
+
+    @GetMapping
+    public ResponseEntity<List<EquipamentoDTO>> getEquipamentos() {
+        return ResponseEntity.ok(equipamentoService.getEquipamentos().stream().map(EquipamentoDTO::new).toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DetalheEquipamentoDTO> getEquipamentoById(@PathVariable Integer id) {
+        return ResponseEntity.ok(new DetalheEquipamentoDTO(equipamentoService.getEquipamentoById(id)));
+    }
+
+    @PostMapping
+    public ResponseEntity<EquipamentoDTO> createEquipamento(@RequestBody @Valid CadastroEquipamentoDTO cadastroEquipamentoDTO) {
+        return ResponseEntity.ok(new EquipamentoDTO(equipamentoService.createEquipamento(cadastroEquipamentoDTO)));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EquipamentoDTO> updateEquipamento(@PathVariable Integer id, @RequestBody UpdateEquipamentoDTO updateEquipamentoDTO) {
+        return ResponseEntity.ok(new EquipamentoDTO(equipamentoService.updateEquipamento(id, updateEquipamentoDTO)));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEquipamento(@PathVariable Integer id) {
+        equipamentoService.deleteEquipamento(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}/historico-manutencao")
+    public ResponseEntity<List<ManutencaoDTO>> getManutencoes(@PathVariable Integer id) {
+        return ResponseEntity.ok(equipamentoService.getManutencoes(id).stream().map(ManutencaoDTO::new).toList());
+    }
+}
---
+++ src/main/java/org/projeto/tde/projetoemprestimotde/services/EquipamentoService.java
@@ -0,0 +1,59 @@
+package org.projeto.tde.projetoemprestimotde.services;
+
+import org.projeto.tde.projetoemprestimotde.DTOs.Equipamento.CadastroEquipamentoDTO;
+import org.projeto.tde.projetoemprestimotde.DTOs.Equipamento.UpdateEquipamentoDTO;
+import org.projeto.tde.projetoemprestimotde.entities.Equipamento;
+import org.projeto.tde.projetoemprestimotde.entities.Manutencao;
+import org.projeto.tde.projetoemprestimotde.repositories.EquipamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EquipamentoService {
+
+    @Autowired
+    private EquipamentoRepository equipamentoRepository;
+
+
+    public List<Equipamento> getEquipamentos() {
+        return equipamentoRepository.findAll();
+    }
+
+    public Equipamento getEquipamentoById(Integer id) {
+        return equipamentoRepository.getReferenceById(id);
+    }
+
+    public Equipamento createEquipamento(CadastroEquipamentoDTO cadastroEquipamentoDTO) {
+        Equipamento equipamento = new Equipamento(cadastroEquipamentoDTO);
+        equipamentoRepository.save(equipamento);
+        return equipamento;
+    }
+
+    public Equipamento updateEquipamento(Integer id, UpdateEquipamentoDTO equipamentoDTO) {
+        Equipamento equipamento = equipamentoRepository.getReferenceById(id);
+        equipamento.setNome(equipamentoDTO.nome());
+        equipamento.setDescricao(equipamentoDTO.descricao());
+        equipamento.setDataCompra(equipamentoDTO.dataCompra());
+        equipamento.setPeso(equipamentoDTO.peso());
+        equipamento.setLargura(equipamentoDTO.largura());
+        equipamento.setComprimento(equipamentoDTO.comprimento());
+        equipamento.setEstadoConservacao(equipamentoDTO.estadoConservacao());
+        equipamentoRepository.save(equipamento);
+        return equipamento;
+    }
+
+    public void deleteEquipamento(Integer id) {
+        Equipamento equipamento = equipamentoRepository.getReferenceById(id);
+        if (!equipamento.isDisponivel()) {
+            throw new RuntimeException("Equipamento não pode ser deletado, pois está emprestado");
+        }
+        equipamentoRepository.delete(equipamento);
+    }
+
+    public List<Manutencao> getManutencoes(Integer id) {
+        Equipamento equipamento = equipamentoRepository.getReferenceById(id);
+        return equipamento.getHistoricoManutencao();
+    }
+}
