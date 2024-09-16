f5850cb149f7a469f5896bae6a6c1ae9c0ad9205
joaoarthur <joaoabritto.dev@gmail.com>
2024-09-16 01:16:03 -0300

    feat: Added Spring Boot files

---
+++ src/main/java/org/projeto/tde/projetoemprestimotde/ProjetoEmprestimoTdeApplication.java
@@ -0,0 +1,13 @@
+package org.projeto.tde.projetoemprestimotde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEmprestimoTdeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoEmprestimoTdeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/projeto/tde/projetoemprestimotde/ProjetoEmprestimoTdeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.projeto.tde.projetoemprestimotde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEmprestimoTdeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
